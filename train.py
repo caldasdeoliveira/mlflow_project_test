fb329606094d7f5d234824b3ce835aad6443e95e
caldasdeoliveira <diogo@loka.com>
2020-11-27 10:26:05 +0000

    svc first implementation

--- train.py
+++ train.py
@@ -3,22 +3,22 @@ import numpy as np
 import argparse
 import os
 
-
-from tensorflow import keras
-from tensorflow.keras import layers
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 import mlflow
-import mlflow.keras
+import mlflow.sklearn
 
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
 from utils import load_data_folder
 
 def eval_metrics(actual, pred):
-  f1 = f1_score(actual, pred)
-  acc = accuracy_score(actual, pred)
-  roc_auc = roc_auc_score(actual, pred)
-  return f1, acc, roc_auc
+  metrics = {}
+  metrics["f1"] = f1_score(actual, pred)
+  metrics["acc"] = accuracy_score(actual, pred)
+  metrics["roc_auc"] = roc_auc_score(actual, pred)
+  return metrics
 
 
 if __name__ == "__main__":
@@ -27,8 +27,9 @@ if __name__ == "__main__":
     parser.add_argument('--data','-dt', type=str)
     parser.add_argument('--batch_size','-b', type=int)
     parser.add_argument('--epochs','-e', type=int)
-    parser.add_argument('--alpha','-a', type=float)
-    parser.add_argument('--dropout','-d', type=float)
+    parser.add_argument('--C','-c', type=float)
+    parser.add_argument('--kernel','-k', type=str)
+    parser.add_argument('--gamma','-g', type=str)
     parser.add_argument('--val_split','-v', type=float)
 
     args = parser.parse_args()
@@ -37,35 +38,27 @@ if __name__ == "__main__":
     batch_size = args.batch_size
     epochs = args.epochs
     val_split = args.val_split
-    alpha = args.alpha
-    dropout = args.dropout
+    c = args.C 
+    gamma = args.gamma
+    kernel = args.kernel
 
     params = {"batch_size": batch_size, "epochs": epochs, "val_split": val_split, "dropout": dropout}
     
     (x_train, y_train), (x_test, y_test) = load_data_folder(data)
     input_shape = x_train.shape[1:]
 
-    with mlflow.start_run(experiment_id=1):
+    with mlflow.start_run():
 
         mlflow.log_params(params)
-        mlflow.keras.autolog()
-        model = keras.Sequential([
-            keras.Input(shape = input_shape),
-            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Flatten(),
-            layers.Dropout(dropout),
-            layers.Dense(64, activation="relu"),
-            layers.Dropout(dropout),
-            layers.Dense(10, activation="softmax"),
-        ])
+        mlflow.sklearn.autolog()
 
-        model.summary()
-
-        opt =keras.optimizers.Adam(learning_rate = alpha)
-
-        model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy", "AUC"])
+        X_train, X_val, y_train, y_val = train_test_split(x_train, y_train, test_size=val_split, random_state=42)
+        
+        model = SVC(C=C, kernel=kernel, gamma=gamma)
 
         results = model.fit( x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=val_split)
+
+        metrics = eval_metrics(y_ val, model.predict(X_val))
+
+        mlflow.log_metrics(metrics)
+
