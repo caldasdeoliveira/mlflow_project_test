1cf4ae234adc07cac09d7fd201bc31b61b9ae8b4
caldasdeoliveira <diogo@loka.com>
2020-11-27 11:35:17 +0000

    change to ovr and linearSVC

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import numpy as np
 import argparse
 import os
 
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 
 import mlflow
@@ -15,7 +15,7 @@ from utils import load_data_folder
 
 def eval_metrics(actual, pred):
   metrics = {}
-  metrics["f1"] = f1_score(actual, pred)
+  #metrics["f1"] = f1_score(actual, pred)
   metrics["acc"] = accuracy_score(actual, pred)
   metrics["roc_auc"] = roc_auc_score(actual, pred)
   return metrics
@@ -26,8 +26,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data','-dt', type=str)
     parser.add_argument('--C','-c', type=float)
-    parser.add_argument('--kernel','-k', type=str)
-    parser.add_argument('--gamma','-g', type=str)
+    parser.add_argument('--dual','-d', type=bool)
+    parser.add_argument('--penalty','-p', type=str)
     parser.add_argument('--val_split','-v', type=float)
 
     args = parser.parse_args()
@@ -35,21 +35,24 @@ if __name__ == "__main__":
     data = args.data
     val_split = args.val_split
     C = args.C 
-    gamma = args.gamma
-    kernel = args.kernel
+    dual = args.dual
+    penalty = args.penalty
 
-    params = { "val_split": val_split, "C": C, "gamma": gamma, "kernel": kernel}
+    params = { "val_split": val_split, "C": C, "dual": dual, "penalty": penalty}
     
     (x_train, y_train), (x_test, y_test) = load_data_folder(data)
 
-    with mlflow.start_run():
+    with mlflow.start_run(experiment_id=1):
 
         mlflow.log_params(params)
         mlflow.sklearn.autolog()
 
+        x_train = x_train.reshape((x_train.shape[0], 28*28))
+        y_train = y_train.argmax(1)
+
         X_train, X_val, Y_train, Y_val = train_test_split(x_train, y_train, test_size=val_split, random_state=42)
         
-        model = SVC(C=C, kernel=kernel, gamma=gamma)
+        model = LinearSVC(C=C, dual=dual, penalty=penalty)
 
         results = model.fit( X_train, Y_train)
 
