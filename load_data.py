a05e9b4106697ddd296c3f96a2bd0142ae7c02d7
caldasdeoliveira <diogo@loka.com>
2020-12-22 13:13:50 +0000

    remove args from load data

--- load_data.py
+++ load_data.py
@@ -33,10 +33,7 @@ def load_data():
 
 if __name__ == "__main__":
     with mlflow.start_run() as mlrun:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--data_destination','-dtd', type=str)
-        args = parser.parse_args()
-        dir_path = tempfile.mkdtemp(dir=args.data_destination)
+        dir_path = tempfile.mkdtemp()
         (x_train, y_train), (x_test, y_test) = load_data()
         paths = {"x_train" : os.path.join(dir_path,"x_train.npy"),
         "y_train": os.path.join(dir_path,"y_train.npy"),
